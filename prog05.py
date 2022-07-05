da21b926c13506d813437f66a5f9fbcb03370828
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2022-07-05 01:04:36 -0300

    Criando programa 5 para leitura dos dados da coleção pmu do mongo e criando uma nova coleção, reduzindo as colunas

--- prog05.py
+++ prog05.py
@@ -2,39 +2,138 @@ import pandas as pd
 import time
 import pymongo
 import json
+import numpy as np
 
 # Buscar dados de 3 PMUs:
-# Ver os campos de cada documento:
-'''
-Serão observados os campos, retirando o 
-    _id, index, Date, Time (America/Sao_Paulo), Status, Frenquency, df/dt,
-    pmu1, pmu2, dataRefPMUini, dataRefPMUFim, nomeArquivo, DAtaHora, DataHora2
-'''
-# Gerar uma tabela contendo:
-'''
-    index, Date, Time (America/SaoPaulo), Status, Frenquency, df/dt,
-    pmu1, DataHora2,
-    Tipo1, Tipo2, Tipo3, Angle, Magnitude:
-    Ex.: (VA1 V_BARRA1_230 == Tipo1 VA1; Tipo2 V_BARRA1; Tipo3 230)
-'''
 
+# -------------------------------------
+# Conexão com o MongoDB:
 mngClient = pymongo.MongoClient('mongodb://localhost:27017/')
 mngDb = mngClient['local'] #mngClient.local
 mngPMU = mngDb["pmu"]
+dbCon = mngDb["pmuDiv"]
+
+'''
+# exportando de DB para csv:
+pmuDiv = dbCon.find({})
+pmuDiv = pd.DataFrame(pmuDiv)
+pmuDiv.to_csv("/home/eduardo/Documentos/UNIFEI/pmuDiv.csv")
+'''
 
 pmu = mngPMU.find({"pmu1" : {"$in" : ['C37118-1000', 'C37118-1001','C37118-1003']}})
 pmu = pd.DataFrame(pmu)
 
-columnsNewDF = ['index', 'Date', 'Time (America/Sao_Paulo)', 
+# -------------------------------------
+
+columnsNewDF = ['id', 'index', 'Date', 'Time (America/Sao_Paulo)', 
                 'Status', 'Frequency', 'df/dt', 'pmu1', 'DataHora2', 
-               'Tipo1', 'Tipo2', 'Tipo3', 'Angle', 'Magnitude']
+                'Tipo1', 'Tipo2', 'Tipo3', 'Angle', 'Magnitude']
 
-dados = ['1','27/06/2022', '08:00', '02 00', 60.02, -0.004289, 'C37118-1000','20210408_060000.000000', 
-         'VA1', 'V_BARRA1','230', 235609.765625,95.99189]
+colunasPadrao = ['_id', 'index', 'Date', 'Time (America/Sao_Paulo)', 
+                 'Status', 'Frequency', 'df/dt', 'pmu1', 'DataHora2', 
+                 'Tipo1', 'Tipo2', 'Tipo3', 'Angle', 'Magnitude', 'pmu2', 'dataRefPMUIni',
+                 'dataRefPMUFim', 'nomeArquivo', 'DataHora']
 
-#pmuNew = pd.DataFrame.columns(columnsNewDF)
+iteracao = 0
+indexLine = 0
 pmuNew = pd.DataFrame(columns=columnsNewDF)
-    
+for index, row in pmu.iterrows():
+    #Zerando variáveis a cada iteração
+    interaCol       = 0   
+    newRow          = []
+    newRowInst      = []
+    primaParte      = []
+    newRowCol       = []
+    newRowColInsert = []
+    primaParteCol   = []    
+    #loop nas colunas:
+    for coluna in pmu.columns:        
+        # verificação se a 'coluna' está na lista de colunas padrão
+        if not (coluna in colunasPadrao):
+            # verificação se o valor da coluna é null, caso sim, próximo
+            if not (pd.isnull(row[coluna])):                
+                txtColuna  = coluna.split(" ")                
+                txtColuna2 = txtColuna[1].split("_")
+                
+                if(interaCol == 0):                
+                    newRowCol.append("Tipo1")
+                    newRow.append(txtColuna[0])
+
+                    newRowCol.append("Tipo2")
+                    newRow.append(txtColuna2[0] + "_" + txtColuna2[1])
+                    
+                    if(len(txtColuna2) > 2):
+                        newRowCol.append("Tipo3")
+                        newRow.append(txtColuna2[2])
+
+                if(txtColuna[2].upper().__eq__('ANGLE')):
+                    newRowCol.append('Angle')
+                    newRow.append(row[coluna])
+                    interaCol += 1            
+                if(txtColuna[2].upper().__eq__('MAGNITUDE')):
+                    newRowCol.append('Magnitude')
+                    newRow.append(row[coluna])
+                    interaCol += 1
+        else:            
+            if not (coluna.upper().__eq__('_ID')):
+                primaParteCol.append(coluna)
+                primaParte.append(row[coluna])            
+        if(interaCol == 2):
+            #Verificando se já existe os campos de DataHora2 e pmu1 nos arrays:
+            existData = False
+            existPmu  = False
+            for c in newRowCol:
+                if (c.rstrip().lstrip().upper().__eq__('DATAHORA2')):
+                    existData = True
+                if (c.rstrip().lstrip().upper().__eq__('PMU1')):
+                    existPmu = True
+
+            if (not existData) and (not existPmu):
+                for c in primaParteCol:
+                    if (c.rstrip().lstrip().upper().__eq__('DATAHORA2')):
+                        existData = True
+                    if (c.rstrip().lstrip().upper().__eq__('PMU1')):
+                        existPmu = True
+            
+            #Caso não existir, acrescentar os campos DataHora2 e pmu1:
+            if not (existData):
+                newRow.append(row['DataHora2'])
+                newRowCol.append('DataHora2')
+            
+            if not (existPmu):
+                newRow.append(row['pmu1'])
+                newRowCol.append('pmu1')
+
+            #Criando campo primary_key:
+            indexLine += 1
+            newRowCol.append('id')
+            newRow.append(indexLine)
+
+            #Concatenando a lista da 1a parte e a lista da segunda parte de campos:
+            newRowInst      = np.concatenate((primaParte, newRow))
+            newRowColInsert = np.concatenate((primaParteCol, newRowCol))    
+
+            #Inserindo no DataFrame novo
+            pmuColumn = pd.DataFrame([newRowInst], columns=newRowColInsert)
+            pmuNew = pd.concat([pmuNew, pmuColumn], ignore_index=True, axis=0)
+            interaCol       = 0
+            newRow          = []
+            newRowInst      = []
+            newRowCol       = []
+            newRowColInsert = []
+
+    #Inserindo registro no banco de dados // pode substituir por arquivo:
+    iteracao += 1
+    print('>>>> ' + str(iteracao) + '/' + str(pmu.count()[0]))
+    pmuNew.set_index('id')    
+    dataJson = pmuNew.to_json(orient="table") 
+    vlrPreInsert = json.loads(dataJson);            
+    vlrInsert = vlrPreInsert['data']            
+    #dbCon.insert_one(vlrInsert)
+    dbCon.insert_many(vlrInsert)
+    pmuNew = pd.DataFrame(columns=columnsNewDF)
+
+mngClient.close()       
 '''
 for index, row in pmu.iterrows():  
     row.  
@@ -43,10 +142,6 @@ for index, row in pmu.iterrows():
              ]   
     print(dados) 
 '''
-for row in pmu.itertuples():
-    print(row.columns)
-print(pmuNew)
-
 x= 1
 
 
